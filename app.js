c306193cb88af558a4e25932be46d75abbe4b5ed
StechAnurag <anuragtech24.7@gmail.com>
2019-09-21 17:58:34 +0530

    handling errors with global error handler middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,5 +18,22 @@ app.use(express.static(`${__dirname}/public`));
 app.get('/', (req, res) => res.send('<h1>Welcome to Natours</h1>'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// 404 - NOT FOUND ROUTE
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Reuested resource, ${req.originalUrl} not found!`
+  // });
+
+  // const err = new Error(`Reuested resource, ${req.originalUrl} not found!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+  // next(err);
+  next(new AppError(`Reuested resource, ${req.originalUrl} not found!`, 404));
+});
+
+// GLOBAL ERROR HANDLING Middlware
+app.use(globalErrorHandler);
 
 module.exports = app;
