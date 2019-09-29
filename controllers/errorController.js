564181905a31f69d51353eb895903465eace0493
StechAnurag <anuragtech24.7@gmail.com>
2019-09-29 21:34:43 +0530

    sending errors in production environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,49 @@
-module.exports = (err, req, res, next) => {
-  // console.log(err.stackTrace);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Inavlid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    message: err.message,
+    error: err,
+    stack: err.stack
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // Programming or other unknown error : don't leak error details
+  } else {
+    // 1) Log the Error
+
+    // 2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
