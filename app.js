1bfd121d1d4f3216c09654da91209f28ee1ce5cb
Anurag <anurag.kumar@feryservices.com>
2020-03-08 17:25:55 +0530

    enable cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -20,12 +21,25 @@ const bookingRouter = require('./routes/bookingRoutes');
 const app = express();
 
 // heroku speicific
-app.enable('trus proxy');
+app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// enable CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// app.use(cors({
+//  origin : 'https://natours.com'
+// }))
+
+// Respond to OPTIONS request by browser -- here app.options means any http verb eg, GET, POST etc
+app.options('*', cors());
+
+// optionally enable OPTIONS request to be responded on a particular route
+// app.options('/api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(`${__dirname}/public`));
